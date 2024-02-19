298cdbe2941ba6724e2a9cb2151d4d804ce7f74e
beircheart17 <103154477+beircheart17@users.noreply.github.com>
2024-02-19 11:37:49 -0600

    1

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './books.html', './blog.html', 'info.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
